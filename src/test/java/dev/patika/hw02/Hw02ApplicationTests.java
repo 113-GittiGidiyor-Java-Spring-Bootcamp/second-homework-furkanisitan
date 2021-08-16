49d461af414342319e7ceccb489902a1c8cd9aed
Furkan Işıtan <furkanisitan96@gmail.com>
2021-08-16 06:32:39 +0300

    Create spring boot project

---
+++ src/test/java/dev/patika/hw02/Hw02ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patika.hw02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
