f9c48efe953ca90ab54c20cde1f2dd3bb119b822
Furkan Işıtan <furkanisitan96@gmail.com>
2021-08-17 20:57:36 +0300

    Create ResponseMessage.java

---
+++ src/main/java/dev/patika/hw02/core/utilities/constants/ResponseMessage.java
@@ -0,0 +1,10 @@
+package dev.patika.hw02.core.utilities.constants;
+
+/**
+ * This class contains constant response messages for api.
+ */
+public class ResponseMessage {
+
+    public static final String NOT_FOUND = "No Records Found.";
+
+}
